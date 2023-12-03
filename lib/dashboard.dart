89be27c7076a3890b504fc9339a2c48da12e9431
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-03 17:36:44 +0530

    added dashboard.dart a

---
+++ lib/dashboard.dart
@@ -0,0 +1,25 @@
+import 'main.dart';
+import 'package:flutter/material.dart';
+
+class dashboard extends StatefulWidget {
+  const dashboard({Key? key}) : super(key: key);
+
+  @override
+  State<dashboard> createState() => _dashboardState();
+}
+
+class _dashboardState extends State<dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        body: Container(
+
+        ) ,
+      ),
+    )
+  }
+}
